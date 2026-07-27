c6fd3032d7f170526f418beb783d31d894b87b6f
QuangHao2011 <secretssrt@gmail.com>
2026-07-27 19:30:15 +0700

    Add files via upload

---
+++ writeup/floating-point_number/binaries_test/dump_floating_point_fraction.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+	float x = 1.50; //binary = 1.10
+	printf("dump fration 23bit : %.23f\n",x);
+}
---
+++ writeup/floating-point_number/binaries_test/dump_floating_point_fraction2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+	float x = 1.50; //binary = 1.10
+	float y = 1.25; //binary = 1.01
+	printf("dump fration 23bit : %.23f\n",x + y); //phần này sẽ biểu diễn số hữu hạn
+}
